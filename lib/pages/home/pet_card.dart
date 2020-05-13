87c58dcbd2c571d977dbb68ec1d42e8efaca6484
XiaoYang <mikexfreeze@gmail.com>
2020-05-13 11:53:20 +0800

    列表图片

--- lib/pages/home/pet_card.dart
+++ lib/pages/home/pet_card.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 class PetCard extends StatelessWidget {
   @override
   Widget build(context) {
-    return Text('test');
+    final imageWidget = AspectRatio(
+      aspectRatio: 3/2,
+      child: FittedBox(
+        fit: BoxFit.cover,
+        child: Image(
+          image: AssetImage('assets/exmaple/pet_images/0-0.jpg'),
+        ),
+      )
+    );
+
+    return Container(
+      color: Colors.black38,
+      child: Column(
+        children: [
+          imageWidget
+
+        ]
+      ),
+    );
   }
 }
